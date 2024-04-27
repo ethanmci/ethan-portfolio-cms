7f6f38a1194518762213391f4f8d35599746c654
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-04-27 14:53:57 -0400

    init changes

---
+++ schemaTypes/project.ts
@@ -0,0 +1,62 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'date',
+    }),
+    defineField({
+      name: 'cover',
+      title: 'Cover',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'summary',
+      title: 'Summary',
+      type: 'string',
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'carousel',
+      title: 'Carousel',
+      type: 'array',
+      of: [{type: 'image'}],
+    }),
+    defineField({
+      name: 'links',
+      title: 'Links',
+      type: 'array',
+      of: [{type: 'url'}],
+    }),
+  ],
+  orderings: [
+    {
+      title: 'Release Date, New',
+      name: 'releaseDateDesc',
+      by: [{field: 'date', direction: 'desc'}],
+    },
+    {
+      title: 'Release Date, Old',
+      name: 'releaseDateAsc',
+      by: [{field: 'date', direction: 'asc'}],
+    },
+  ],
+})
